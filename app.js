2f9b586f48d9f349e1375aec2515f2c703505138
bitcollector <sadullahiendrit@gmail.com>
2017-05-26 00:42:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Bitcollectors v0.0.8
-
-
-var config = {
 cd untitled-dice
 npm install
 python -m SimpleHTTPServer 5000
+
+
+var config = {
+
   // - Your app's id on moneypot.com
   app_id: 18,
   // - Displayed in the navbar
