4dff6cc7ac7c1ced6215bc9deaf7704086d2f627
bitcollector <sadullahiendrit@gmail.com>
2017-05-26 00:30:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 18,
   // - Displayed in the navbar
   app_name: 'Bitcollectors',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: 
   redirect_uri: 'https://bitcollectors.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
